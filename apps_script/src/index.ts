2097981e0fd832e675abbc1ee4bcd4355fb6d61f
Barry Molina <bazzaboy@gmail.com>
2022-08-18 00:00:25 -0500

    remove implicit anys

--- apps_script/src/index.ts
+++ apps_script/src/index.ts
@@ -1,25 +1,55 @@
+import GmailMessage = GoogleAppsScript.Gmail.GmailMessage
+import Sheet = GoogleAppsScript.Spreadsheet.Sheet
+
 const receipt_num_col = 3
 const tracking_num_col = 5
+
+/**
+ * Receipt data gathered from email
+ */
+interface ReceiptInfo {
+  messageId?: string
+  date?: string
+  receiptNum?: string
+  total?: string
+  trackingNum?: string
+}
+
+/** 
+ * Google Sheets column numbers are 1-based
+ */
+type ColumnNumber = number
+
+/** 
+ * Google Sheets row numbers are 1-based
+ */
+type RowNumber = number
+
+/**
+ * Multi-dimensional array of data representing a Google Sheet
+ */
+type SheetData = string[][]
+
 
 function getReceiptInfoFromGmail() {
   //var receiptThreads = GmailApp.getUserLabelByName('Home Depot/Receipts').getThreads()
   var receiptThreads = GmailApp.search('label:home-depot-receipts-not-applied is:read')
-  let receiptMessages = []
+  let receiptMessages: GmailMessage[] = []
   receiptThreads.forEach((t) => {
     receiptMessages = [...receiptMessages, ...t.getMessages()]
   })
   Logger.log(receiptMessages.length)
   const numDateRe = /(\d{4}\s{2}\d{5}\s{2}\d{5})\s+(\d{2}\/\d{2}\/\d{2})/
   const totalRe = /\s*TOTAL\s+\$(\d+.\d{2})/
-  const receiptsInfo = receiptMessages.reduce((receiptsInfo, message) => {
+  const receiptsInfo: ReceiptInfo[] = receiptMessages.reduce<ReceiptInfo[]>((receiptsInfo, message) => {
     const body = message.getPlainBody()
     const numDateMatch = body.match(numDateRe)
     const totalMatch = body.match(totalRe)
-    if (totalMatch) {
-      let receiptInfo = {
+    if (totalMatch && numDateMatch) {
+      let receiptInfo: ReceiptInfo = {
         messageId: message.getId(),
-        receiptNum: numDateMatch ? numDateMatch[1].replace(/ /g, '') : null,
-        date: numDateMatch ? numDateMatch[2] : null,
+        receiptNum: numDateMatch[1].replace(/ /g, ''),
+        date: numDateMatch[2],
         total: totalMatch[1]
       }
       return [...receiptsInfo, receiptInfo]
@@ -29,8 +59,7 @@ function getReceiptInfoFromGmail() {
   return receiptsInfo
 }
 
-//TODO: check for error calling function
-function submitReceipt(receiptData) {
+function submitReceipt(receiptData: ReceiptInfo): string | null {
   const functionUrl = 'https://homedepot-rebate-2e7shldrnq-uc.a.run.app'
   const token = ScriptApp.getIdentityToken()
   var options = {
@@ -54,28 +83,11 @@ function submitReceipt(receiptData) {
   return null
 }
 
-function testCallFunction() {
-  const receiptData = {
-    date: '07/26/22',
-    total: '8.19',
-    receiptNum: '49410005243829'
-  }
-  const tracking = submitReceipt(receiptData)
-  Logger.log(tracking)
-}
-
-function writeToSheet(receiptData) {
-  var sheet = SpreadsheetApp.getActiveSheet()
-  receiptData.forEach(row => {
-    sheet.appendRow(row)
-  })
-}
-
 function writeReceiptsToSheet() {
   const receipts = getReceiptInfoFromGmail()
   var sheet = SpreadsheetApp.getActiveSheet()
   sheet.clear()
-  sheet.appendRow(['messageId', 'date', 'receiptNum', 'total'])
+  sheet.appendRow(['messageId', 'date', 'receiptNum', 'total', 'trackingNum'])
   receipts.forEach(receipt => {
     sheet.appendRow([receipt.messageId, receipt.date, receipt.receiptNum, receipt.total])
   })
@@ -85,12 +97,14 @@ function submitManyFromSheet() {
 
 }
 
-function submitFromSheet(sheet, receiptInfo) {
-  Logger.log('submitting: ' + receiptInfo)
+function submitFromSheet(sheet: Sheet, receiptInfo: ReceiptInfo) {
   const trackingNum = submitReceipt(receiptInfo)
-  if (trackingNum) {
+  if (trackingNum && receiptInfo.receiptNum) {
     Logger.log(trackingNum)
     setTrackingNum(sheet, receiptInfo.receiptNum, trackingNum)
+  } else {
+    Logger.log('Error submitting receipt: ')
+    Logger.log(receiptInfo)
   }
 }
 
@@ -104,65 +118,49 @@ function testSubmitFromSheet() {
 function getReceiptInfoFromSheet(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
   var sheet = SpreadsheetApp.getActiveSheet()
   var data = sheet.getDataRange().getDisplayValues()
-  var headers = getHeaders(sheet)
-  var receipts = []
+  var headers = data[0]
+  var receipts: ReceiptInfo[] = []
+  // skip first row (headers)
+  // For each row, create a receipt object
   for (var i = 1; i < data.length; i++) {
-    var row = data[i].reduce((cols, col, x) => {
+    var receipt = data[i].reduce<ReceiptInfo>((cols, col, x) => {
       return { ...cols, [headers[x]]: col }
     }, {})
-    receipts.push(row)
+    receipts.push(receipt)
   }
   return receipts
 }
 
-function getHeaders(sheet) {
-  var data = sheet.getDataRange().getDisplayValues()
-  var headers = data[0]
-  return headers
-}
 
-// Get the row containing the specified value in a given column (col index starts at 1)
-function getRowByColValue(data, value, col) {
+// Get the row containing the specified value in a given column (starting at 1)
+function getRowByColValue(data: SheetData, value: string, col: ColumnNumber): RowNumber | null {
   for (var i = 1; i < data.length; i++) {
     Logger.log('testing ' + data[i][col - 1] + ' == ' + value)
     if (data[i][col - 1] == value) {
       return i + 1
     }
   }
+  Logger.log('Unable to find row for ' + value)
   return null
 }
 
-function testGetRowByColValue() {
-  var sheet = SpreadsheetApp.getActiveSheet()
-  var col = 2
-  const trackingNumCol = 4
-  var row = getRowByColValue(sheet, col, '49410005124995')
-  Logger.log(row)
-  var r = sheet.getRange(row, trackingNumCol)
-  //Logger.log(r.getDisplayValues())
-  r.setValue('trackingtest')
-}
-
-function getRowByTrackingNum(data, trackingNum) {
+function getRowByTrackingNum(data: SheetData, trackingNum: string): RowNumber | null {
   return getRowByColValue(data, trackingNum, tracking_num_col)
 }
 
-function getRowByReceiptNum(data, receiptNum) {
+function getRowByReceiptNum(data: SheetData, receiptNum: string): RowNumber | null {
   return getRowByColValue(data, receiptNum, receipt_num_col)
 }
 
-function testGetRowByReceiptNum() {
-  var sheet = SpreadsheetApp.getActiveSheet()
-  var data = sheet.getDataRange().getDisplayValues()
-  Logger.log(data)
-  Logger.log(getRowByReceiptNum(data, 49070006255400))
-}
-
-function setTrackingNum(sheet, receiptNum, trackingNum) {
+function setTrackingNum(sheet: Sheet, receiptNum: string, trackingNum: string) {
   var data = sheet.getDataRange().getDisplayValues()
   var row = getRowByReceiptNum(data, receiptNum)
-  var cell = sheet.getRange(row, tracking_num_col)
-  cell.setValue(trackingNum)
+  if (row) {
+    var cell = sheet.getRange(row, tracking_num_col)
+    cell.setValue(trackingNum)
+  } else {
+    Logger.log('Could not find receipt number ' + receiptNum)
+  }
 }
 
 function setRead() {
