60f6096eba316a5119fa9bc90f3a40ae3f64d47d
Barry Molina <bazzaboy@gmail.com>
2024-01-03 09:15:27 -0600

    Rebate application updates
    Update the automation to accomodate changes in rebate application
    process. Home depot no longer gives an option for physical cards,
    only e-gift cards. Also throw a more detailed error if the purchase date
    is invalid.

--- functions/src/rebateApply.js
+++ functions/src/rebateApply.js
@@ -1,120 +1,140 @@
+const puppeteer = require("puppeteer");
 
-const puppeteer = require('puppeteer');
+const rebateApply = async (
+  browser,
+  me,
+  receiptNum,
+  receiptTotal,
+  purchaseDate,
+  physicalGiftCard = true
+) => {
+  validateInput(me, receiptNum, receiptTotal, purchaseDate);
 
-const rebateApply = async (browser, me, receiptNum, receiptTotal, purchaseDate, physicalGiftCard = true) => {
+  let applyState = "start";
 
-	validateInput(me, receiptNum, receiptTotal, purchaseDate)
+  const page = await browser.newPage();
 
-	let applyState = 'start'
+  try {
+    // Navigate to the Home Depot rebate application page
+    applyState = "Navigate to the Home Depot rebate application page";
+    await page.goto("https://www.homedepotrebates11percent.com/#/home", {
+      waitUntil: "networkidle2",
+    });
+    await page.waitForNetworkIdle();
 
-	const page = await browser.newPage();
+    // Fill out purchase date
+    applyState = "Fill out purchase date";
+    await page.type("#purchaseDateOnlyText", purchaseDate);
+    await page.click("#home-offer-purchasedate-continue2");
+    await page.waitForNetworkIdle();
 
-	try {
-		// Navigate to the Home Depot rebate application page
-		applyState = 'Navigate to the Home Depot rebate application page'
-		await page.goto('https://www.homedepotrebates11percent.com/#/home', { waitUntil: 'networkidle2' });
-		await page.waitForNetworkIdle();
+    // Check if purchase date is valid
+    if ((await page.$('strong[ng-bind-html="noPromotionError"]')) !== null) {
+      throw new Error("Invalid purchase date");
+    }
 
-		// Fill out purchase date
-		applyState = 'Fill out purchase date'
-		await page.type('#purchaseDateOnlyText', purchaseDate);
-		await page.click('#home-offer-purchasedate-continue2');
-		await page.waitForNetworkIdle();
+    // Continue
+    await page.click("#continueOrSubmitBtn");
+    await page.waitForNetworkIdle();
 
-		// Continue
-		await page.click('#continueOrSubmitBtn')
-		await page.waitForNetworkIdle();
+    // Enter receipt number and total
+    applyState = "Enter receipt number and total";
+    await page.type("#Receipt\\ Number", receiptNum);
+    await page.type("#Gross\\ Sales", receiptTotal);
+    await page.click("#continueOrSubmitBtn");
+    await page.waitForNetworkIdle();
 
-		// Enter receipt number and total
-		applyState = 'Enter receipt number and total'
-		await page.type('#Receipt\\ Number', receiptNum)
-		await page.type('#Gross\\ Sales', receiptTotal)
-		await page.click('#continueOrSubmitBtn')
-		await page.waitForNetworkIdle();
+    // Enter personal info
+    // For each field, enter the value from the me object
+    applyState = "Enter personal info";
+    await page.type("input[name=firstName]", me.first_name);
+    await page.type("input[name=lastName]", me.last_name);
+    await page.type("input[name=phoneNumber]", me.phone);
+    await page.type("input[name=email]", me.email);
+    await page.type("input[name=confirmEmail]", me.email);
+    await page.type("input[name=address1]", me.street);
+    await page.type("input[name=postalCode]", me.zip);
+    await page.type("input[name=city]", me.city);
+    await page.select("select[name=country]", me.country);
+    await page.select("select[name=state]", me.state);
+    await page.$eval("input#emailOptIn", (check) => (check.checked = false));
 
-		// Select gift card type
-		applyState = 'Select gift card type'
-		const giftCardSelector = physicalGiftCard ? '#The\\ Home\\ Depot\\ Physical\\ Gift\\ Card' : 'The\\ Home\\ Depot\\ eGift\\ Card'
-		await page.click(giftCardSelector)
-		await page.click('#continueOrSubmitBtn')
+    // Use recommended address
+    applyState = "Use recommended address";
+    await page.click("#continueBtn > button");
+    await page.waitForNetworkIdle();
 
-		// Enter personal info
-		// For each field, enter the value from the me object
-		applyState = 'Enter personal info'
-		await page.type('input[name=firstName]', me.first_name)
-		await page.type('input[name=lastName]', me.last_name)
-		await page.type('input[name=phoneNumber]', me.phone)
-		await page.type('input[name=email]', me.email)
-		await page.type('input[name=confirmEmail]', me.email)
-		await page.type('input[name=address1]', me.street)
-		await page.type('input[name=postalCode]', me.zip)
-		await page.type('input[name=city]', me.city)
-		await page.select('select[name=country]', me.country)
-		await page.select('select[name=state]', me.state)
-		await page.$eval('input#emailOptIn', check => check.checked = false)
+    if ((await page.$("div.modal-dialog")) !== null) {
+      await page.click("#recommendedAddressBtn");
+      await page.waitForNetworkIdle();
+    }
 
-		// Use reccomended address
-		applyState = 'Use reccomended address'
-		await page.click('#continueBtn > button')
-		await page.waitForNetworkIdle();
+    // Submit application
+    applyState = "Submitting application";
+    await page.click("#continueOrSubmitBtn");
+    await page.waitForNetworkIdle();
 
-		if (await page.$('div.modal-dialog') !== null) {
-			await page.click('#recommendedAddressBtn')
-			await page.waitForNetworkIdle();
-		}
+    // // Get tracking number
+    applyState = "Getting tracking number";
+    const trackingSelector = await page.$("#confirmation-trackingNumber");
+    const trackingNumber = await (
+      await trackingSelector.getProperty("textContent")
+    ).jsonValue();
+    console.log(`Tracking number: ${trackingNumber}`);
+    await browser.close();
 
-		// Submit application
-		applyState = 'Submitting application'
-		await page.click('#continueOrSubmitBtn')
-		await page.waitForNetworkIdle();
+    return trackingNumber;
+  } catch (error) {
+    const errorPage = page.url();
+    error.message = `${error.message} - ${applyState} - ${errorPage}`;
+    throw error;
+  }
+};
 
-		// // Get tracking number
-		applyState = 'Getting tracking number'
-		const trackingSelector = await page.$('#confirmation-trackingNumber')
-		const trackingNumber = await (await trackingSelector.getProperty('textContent')).jsonValue()
-		// console.log(`Tracking number: ${trackingNumber}`)
-		await browser.close();
-
-		return trackingNumber
-
-	} catch (error) {
-		const errorPage = page.url()
-		error.message = `${error.message} - ${applyState} - ${errorPage}`
-		throw error
-	}
-
-}
-
-// Validate input. 
+// Validate input.
 function validateInput(me, receiptNum, receiptTotal, purchaseDate) {
-	if (!me.first_name || !me.last_name || !me.phone || !me.email || !me.street || !me.zip || !me.city || !me.country || !me.state) {
-		throw new Error('Missing personal info')
-	}
-	if (!receiptNum || !receiptTotal || !purchaseDate) {
-		throw new Error('Missing receipt info')
-	}
+  if (
+    !me.first_name ||
+    !me.last_name ||
+    !me.phone ||
+    !me.email ||
+    !me.street ||
+    !me.zip ||
+    !me.city ||
+    !me.country ||
+    !me.state
+  ) {
+    throw new Error("Missing personal info");
+  }
+  if (!receiptNum || !receiptTotal || !purchaseDate) {
+    throw new Error("Missing receipt info");
+  }
 
-	if (typeof receiptNum !== 'string' || typeof receiptTotal !== 'string' || typeof purchaseDate !== 'string') {
-		throw new Error('Invalid receipt info')
-	}
+  if (
+    typeof receiptNum !== "string" ||
+    typeof receiptTotal !== "string" ||
+    typeof purchaseDate !== "string"
+  ) {
+    throw new Error("Invalid receipt info");
+  }
 
-	// Use regex to check that receiptNum is a string containing 14 digits
-	const receiptNumRegex = /^\d{14}$/
-	if (!receiptNumRegex.test(receiptNum)) {
-		throw new Error('Invalid receipt number')
-	}
+  // Use regex to check that receiptNum is a string containing 14 digits
+  const receiptNumRegex = /^\d{14}$/;
+  if (!receiptNumRegex.test(receiptNum)) {
+    throw new Error("Invalid receipt number");
+  }
 
-	// Use regex to check that receiptTotal is a string containing one or more digits followed by a period and two more digits
-	const receiptTotalRegex = /^\d+\.\d{2}$/
-	if (!receiptTotalRegex.test(receiptTotal)) {
-		throw new Error('Invalid receipt total')
-	}
+  // Use regex to check that receiptTotal is a string containing one or more digits followed by a period and two more digits
+  const receiptTotalRegex = /^\d+\.\d{2}$/;
+  if (!receiptTotalRegex.test(receiptTotal)) {
+    throw new Error("Invalid receipt total");
+  }
 
-	// Use regex to check that purchaseDate is a string containing two digits for the month and two digits for the day and two digits for the year
-	const purchaseDateRegex = /^\d{2}\/\d{2}\/\d{2}$/
-	if (!purchaseDateRegex.test(purchaseDate)) {
-		throw new Error('Invalid purchase date')
-	}
+  // Use regex to check that purchaseDate is a string containing two digits for the month and two digits for the day and two digits for the year
+  const purchaseDateRegex = /^\d{2}\/\d{2}\/\d{2}$/;
+  if (!purchaseDateRegex.test(purchaseDate)) {
+    throw new Error("Invalid purchase date");
+  }
 }
 
 exports.rebateApply = rebateApply;
--- functions/tests/testRebateApply.js
+++ functions/tests/testRebateApply.js
@@ -1,19 +1,25 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-const { rebateApply } = require('../src/rebateApply.js')
-const { me } = require('../me.js')
+const { rebateApply } = require("../src/rebateApply.js");
+const { me } = require("../me.js");
 
 async function testRebateApply() {
-	const browser = await puppeteer.launch({
-		headless: false,
-		// sloMo: 250
-	});
+  const browser = await puppeteer.launch({
+    headless: false,
+    // sloMo: 250
+  });
 
-	// rebateApply(browser, me, '49410006299184', '14.65', '08/05/22')
-	// rebateApply(browser, me, '49410005189204', '8.94', '08/04/22')
-	// rebateApply(browser, me, '49410005275508', '20.76', '08/04/22')
-	// rebateApply(browser, me, '49070006127278', '26.31', '08/03/22')
-	const tracking = rebateApply(browser, me, '49070005292917', '21.01', '08/03/22')
+  // rebateApply(browser, me, '49410006299184', '14.65', '08/05/22')
+  // rebateApply(browser, me, '49410005189204', '8.94', '08/04/22')
+  // rebateApply(browser, me, '49410005275508', '20.76', '08/04/22')
+  // rebateApply(browser, me, '49070006127278', '26.31', '08/03/22')
+  const tracking = rebateApply(
+    browser,
+    me,
+    "49410005178025",
+    "19.04",
+    "12/19/23"
+  );
 }
 
-testRebateApply()
+testRebateApply();
